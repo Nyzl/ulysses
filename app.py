33d9f9726db78333fc00ecb33cd5f03e94ef941a
Ian Ansell <ian.ansell.uk@gmail.com>
2020-10-01 08:11:23 +0100

    var urls changed to url

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def index():
 @app.route('/index',methods=['POST'])
 def index2():
 	app.vars['url'] = request.form['url']
-	urls = str(app.vars['url'])
-	ulysses.ithaca(urls)
+	url = str(app.vars['url'])
+	ulysses.ithaca(url)
 
 	title = ulysses.title
 	wcount = ulysses.wcount
